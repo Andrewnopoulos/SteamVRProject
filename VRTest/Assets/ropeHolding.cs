4f43b7cb8361604fe6297fd23246c1dffefd26fd
Andrew <andrew.giannopoulos@gmail.com>
2016-01-12 23:42:53 +1100

    Added a test floor and rope

---
+++ VRTest/Assets/ropeHolding.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ropeHolding : MonoBehaviour {
+
+    Transform anchorTransform;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    void Awake()
+    {
+        anchorTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = anchorTransform.position;
+        transform.rotation = anchorTransform.rotation;
+	}
+}
